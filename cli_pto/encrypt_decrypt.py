5726fe4ac07e656f5b455a6636cfed60ef34ba90
Özenç Bilgili <bilgiliozenc@gmail.com>
2020-07-28 16:33:55 +0300

    Add exception for wrong username/password

--- cli_pto/encrypt_decrypt.py
+++ cli_pto/encrypt_decrypt.py
@@ -9,15 +9,15 @@ from Crypto.Util.Padding import pad, unpad
 BLOCK_SIZE = 32
 
 class EncryptDecrypt:
-    def __init__(self, password, filename):
+    def __init__(self, user, password):
+        self.user = user
         self.password = password
-        self.filename = filename
 
     def __generate_key(self):
+        user = self.user
         password = self.password
-        filename = self.filename
 
-        random.seed(filename)
+        random.seed(user)
         salt = ''.join(random.choice(string.ascii_lowercase) for i in range(32))
 
         return PBKDF2(password, salt, 16, 1000, None)
@@ -32,4 +32,7 @@ class EncryptDecrypt:
             return ""
         else:
             cipher = AES.new(self.__generate_key(), AES.MODE_ECB)
-            return unpad(cipher.decrypt(text), BLOCK_SIZE).decode('utf-8')
+            try:
+                return unpad(cipher.decrypt(text), BLOCK_SIZE).decode('utf-8')
+            except:
+                return 'Wrong username or password'
